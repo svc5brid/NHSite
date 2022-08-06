8f4a5b864e68107067d6219e591dd725ce672e74
svc5brid <svc5brid@ezweb.ne.jp>
2022-08-06 11:37:57 +0900

    ajaxのテストをした。数字を送信し、加算,減算をして結果を表示するというもの。 ちゃんと動いた。 Profile作成ぺージでテストしたが、このページには不要だった。

--- Balance_management/urls.py
+++ Balance_management/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "Balance_Management"
 urlpatterns = [
     path('index', views.index, name='index_bm'),
-    path('SelectProfile', views.SelectProfile.as_view(), name="SelectProfile")
+    path('SelectProfile', views.SelectProfile.as_view(), name="SelectProfile"),
+    path('get_profile', views.get_profile, name="get_profile")
     ]
--- Balance_management/views.py
+++ Balance_management/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.http import JsonResponse
 
-def get_profile(request):
-    # 下記情報を使用して、その人が利用可能なプロファイル一覧をリストで返す
-    # request.user()
-    return ["p1", "p2", "p3", "p4"]
-    
+
 # Create your views here.
 def index(request):
     context = {}
@@ -18,7 +15,15 @@ def index(request):
 class SelectProfile(View):
     def get(self, request):
         context = {}
-        context["profile"] = get_profile(request)
         request.session["Balance-Management_Profile"] = "Test"
-        return render(request, "Balance-Management/ProfileSelecter.html", context)
-    
+        return render(request, "Balance-Management/Select_Profile.html", context)
+def get_profile(request):
+    number1 = int(request.POST['number1'])
+    number2 = int(request.POST["number2"])
+    plus = number1 + number2
+    minus = number1 - number2
+    d = {
+        'plus': plus,
+        'minus': minus,
+    }
+    return JsonResponse(d)
