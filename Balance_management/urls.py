52647f64a9e75d8c2b6c99ac235b166deaaf5386
svc5brid <svc5brid@ezweb.ne.jp>
2022-08-07 10:21:51 +0900

    SelectProfileぺージにて、名前の重複確認とUser重複選択防止機構を作成。 Ajaxを利用。

--- Balance_management/urls.py
+++ Balance_management/urls.py
@@ -5,5 +5,8 @@ app_name = "Balance_Management"
 urlpatterns = [
     path('index', views.index, name='index_bm'),
     path('SelectProfile', views.SelectProfile.as_view(), name="SelectProfile"),
-    path('get_profile', views.get_profile, name="get_profile")
+    # Supply when select user at create profile page
+    path('SelectProfile_API', views.SelectProfile_API.as_view(), name="SelectProfile_API"),
+    # path('confirm_useable_profilename', views.select_user, name='confirm_useable_profilename'),
+    # path('select_user1', views.select_user, name='select_user1'),
     ]
--- Balance_management/views.py
+++ Balance_management/views.py
@@ -15,15 +15,64 @@ def index(request):
 class SelectProfile(View):
     def get(self, request):
         context = {}
+        context["Profile"] = [
+            {"id" : "000111222333", "name":"Profile_Test1"},
+            {"id" : "111222333444", "name":"Profile_Test2"},
+            ]
+        context["UserList"] = [
+            {"id":"0070200366", "name":"UserA"},
+            {"id":"0070200367", "name":"UserB"},
+            {"id":"0070200368", "name":"UserC"},
+        ]
         request.session["Balance-Management_Profile"] = "Test"
         return render(request, "Balance-Management/Select_Profile.html", context)
-def get_profile(request):
-    number1 = int(request.POST['number1'])
-    number2 = int(request.POST["number2"])
-    plus = number1 + number2
-    minus = number1 - number2
-    d = {
-        'plus': plus,
-        'minus': minus,
-    }
-    return JsonResponse(d)
+class SelectProfile_API(View):
+    def post(self, request):
+        API_type = request.POST["type"]
+        if API_type == "get_user":
+            json = self.select_user(request)
+        elif API_type == "confirm_name":
+            json = self.confirm_useable_profilename(request)
+        return JsonResponse(json)
+    def select_user(self, request) -> list:
+        user1 = request.POST['user1']
+        # check record exists
+        if user1 in []:
+            pass
+        user2 = {
+            "0070200366":{"id":"0070200366", "name":"UserA"},
+            "0070200367":{"id":"0070200367", "name":"UserB"},
+            "0070200368":{"id":"0070200368", "name":"UserC"},
+        }
+        del user2[user1]
+        user2 = list(user2.values())
+        # if user1 == "UserA":
+        #     user2 = [
+        #         {"id":"0070200367", "name":"UserB"},
+        #         {"id":"0070200368", "name":"UserC"},
+                
+        #         ]
+        # else:
+        #     user2 = [
+        #         {"id":"0070200366", "name":"UserA"},
+        #         {"id":"0070200368", "name":"UserC"},
+                
+        #         ]
+        # return other user
+        d = {
+            "User2": user2
+        }
+        return d
+    def confirm_useable_profilename(self, request) -> bool:
+        profile_name = request.POST["name_profile"]
+        # if inputted profile name already exists at db, return false because duplicate is not supported
+        if profile_name in ["Used_Test"]:
+            d = {
+                "judge": False
+            }
+            return d
+        else:
+            d = {
+                "judge": True
+            }
+            return d
